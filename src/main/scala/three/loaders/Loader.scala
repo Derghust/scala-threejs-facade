d35c119dbc86d30ca05bc3f6efcf5b83768f6b20
Damien Clément <damien.clement.czech@gmail.com>
2024-05-24 17:27:17 +0200

    sync

---
+++ src/main/scala/three/loaders/Loader.scala
@@ -0,0 +1,15 @@
+package org.tesseractrealm.threejsfacade
+package three.loaders
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSImport
+
+/** A simple caching system, used internally by [[FileLoader]].
+ *
+ * [[https://threejs.org/docs/#api/en/loaders/Cache Documentation]]
+ */
+@js.native
+@JSImport("three", "AudioLoader")
+class Loader {
+
+}
