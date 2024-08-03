477c069568926808c369a1583933590398bb854c
Damien Clément <damien.clement.czech@gmail.com>
2024-08-03 19:07:05 +0200

    Update build version and add CSS2DRenderer
    The build version has been updated to r165-s6 in build.sbt. Additionally, a new file CSS2DRenderer.scala is created within three/examples/jsm/renderers path. This new class offers rendering functionalities like getSize, render, and setSize.

--- src/main/scala/three/examples/jsm/renderers/CSS2DRenderer.scala
+++ src/main/scala/three/examples/jsm/renderers/CSS2DRenderer.scala
@@ -3,6 +3,7 @@ package three.examples.jsm.renderers
 
 import org.tesseractrealm.threejsfacade.three.cameras.Camera
 import org.tesseractrealm.threejsfacade.three.core.Object3D
+import org.tesseractrealm.threejsfacade.three.math.Vector2
 import org.tesseractrealm.threejsfacade.three.scenes.Scene
 
 import scala.scalajs.js
@@ -10,7 +11,14 @@ import scala.scalajs.js.annotation.JSImport
 
 @JSImport("three/examples/jsm/renderers/CSS2DRenderer", "CSS2DObject")
 @js.native
-class CSS2DObject extends Object3D {}
+class CSS2DObject extends Object3D:
+  def this(element: js.Dynamic) = this()
+
+  var isCSS2DObject: Boolean = js.native
+  var element: js.Dynamic    = js.native
+  var center: Vector2        = js.native
+
+  def copy(source: CSS2DObject, recursive: Boolean): CSS2DObject = js.native
 
 @JSImport("three/examples/jsm/renderers/CSS2DRenderer", "CSS2DRenderer")
 @js.native
--- src/main/scala/three/math/Vector2.scala
+++ src/main/scala/three/math/Vector2.scala
@@ -6,5 +6,5 @@ import scala.scalajs.js.annotation.JSImport
 
 @js.native
 @JSImport("three", "Vector3")
-class Vector3(var x: Double, var y: Double, var z: Double) extends js.Object:
-  def set(x: Double, y: Double, z: Double): Unit = js.native
+class Vector2(var x: Double, var y: Double) extends js.Object:
+  def set(x: Double, y: Double): Unit = js.native
